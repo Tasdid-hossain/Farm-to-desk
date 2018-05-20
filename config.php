0294dba189d51f38e48fb66325beaffe7ee09853
Tasdid-hossain <101210541@students.swinburne.edu.my>
2018-05-20 20:48:14 +0800

    connection file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '৳';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'farmtodesk';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
