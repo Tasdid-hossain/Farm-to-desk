84e5e9d7c9af9f6e8bbf41f5fc00dda0c23cfef6
isalk <isalkorale@hotmail.com>
2018-05-23 03:03:10 +0800

    promo code

--- cart.php
+++ cart.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 //if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
 if(session_id() == '' || !isset($_SESSION)){session_start();}
@@ -36,6 +37,10 @@ include 'config.php';
     <div class="row" >
       <div class="large-12">
         <?php
+          $promocode = 0;
+           
+              
+            //echo $promocode
 
           echo '<p><h3>Your Shopping Cart</h3></p>';
 
@@ -59,7 +64,7 @@ include 'config.php';
               while($obj = $result->fetch_object()) {
                 $cost = $obj->price * $quantity; //work out the line cost
                 $total = $total + $cost; //add to the total cost
-
+                    
                 echo '<tr>';
                 echo '<td>'.$obj->product_name.'</td>';
 				echo '<td>'.$obj->product_code.'</td>';
@@ -70,29 +75,65 @@ include 'config.php';
             }
 
           }
+              
+//          
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $db = mysqli_connect('localhost', 'root', '', 'farmtodesk');
+
+                
+                    $promoid = mysqli_real_escape_string($db,$_POST['promoid']);
+
+                    $sql = "SELECT promo_value FROM promo WHERE promocode ='$promoid'";
+                    $result = mysqli_query($db,$sql);
+                    $row = mysqli_fetch_assoc($result);
+                    echo $row["promo_value"];
 
 
 
+                    $promocode = $row["promo_value"];
+ }
+                
+           
+
+            $subtotal = 0;
           echo '<tr>';
-          echo '<td colspan="3" align="right">Total</td>';
+          echo '<td colspan="3" align="right">Subtotal</td>';
           echo '<td>'.$total.'</td>';
           echo '</tr>';
+              
+          echo '<tr>';
+          echo '<td colspan="3" align="right">Discount(s)</td>';
+          echo '<td>'.$promocode.'</td>';
+          echo '</tr>';
+              
+              $subtotal = $total - $promocode;
+              
+          echo '<tr>';
+          echo '<td colspan="3" align="right">Total</td>';
+          echo '<td>'.$subtotal.'</td>';
+          echo '</tr>';
+          
+              
            
           echo '<tr>';
 		  $shipping = isset($_SESSION["shipping"]) ? strtolower($_SESSION["shipping"]): "Normal";
-          echo '<td><form method="GET" action="update-cart.php"><H4> Shipping Method </H4><input type="radio" name="shipping" value="Normal" '.($shipping == "Normal" ? "checked='checked'":"").'> 3 Working Days (Free) <br><input type="radio" name="shipping" value="Express" '.($shipping == "Express" ? "checked='checked'":"").'> 1 Working Day Express Shipping (500 BDT)<br><input type="submit" value="Update Shipping"><input type="hidden" name="action" value="shipping"><input type="hidden" name="id" value="'.$product_id.'"></form></td>';
+          echo '<td><form method="GET" action="update-cart.php"><H4> Shipping Method </H4><input type="radio" name="shipping" value="Normal" '.($shipping == "Normal" ? "checked='checked'":"").'> 3 Working Days (Free) <br><input type="radio" name="shipping" value="Express" '.($shipping == "Express" ? "checked='checked'":"").'> 1 Working Day Express Shipping (500 BDT)<br><button type="submit" class="btn" value="Update Shipping">Update Shipping</button><input type="hidden" name="action" value="shipping"><input type="hidden" name="id" value="'.$product_id.'"></form></td>';
           echo '</tr>';
           echo '</br>';
               
               
           echo '<tr>';
           echo '<td colspan="4" align="right"><a href="update-cart.php?action=empty" class="button alert">Empty Cart</a>&nbsp;<a href="products.php" class="button [secondary success alert]">Continue Shopping</a>';
-           
-          echo '<form action="">
+              
+      
+          echo '<form method ="post" action="cart.php">
           <p><b>Promo Code (if applicable)</b></p>
-          <input type="promo" name="promoid">
+          <input type="text" name="promoid">
           <p><font size="1.5">Enter your 5 Digit Code to reedem your discount</font></p>
+          <button type="submit" class="btn" name="promo_code">Add Promo</button>
           <br></form>';
+           
+      
         
               
           if(isset($_SESSION['username'])) {
@@ -121,6 +162,7 @@ include 'config.php';
           echo '</div>';
           ?>
         </div>
+      </div>
 
 
 
@@ -157,5 +199,6 @@ include 'config.php';
     <script>
       $(document).foundation();
     </script>
+      
   </body>
 </html>
