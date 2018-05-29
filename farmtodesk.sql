a4e6e0bd99d278195462942573a8ac2210ffdd73
Tasdid-hossain <101210541@students.swinburne.edu.my>
2018-05-29 13:12:35 +0800

    main database added

--- farmtodesk.sql
+++ farmtodesk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2018 at 10:01 PM
+-- Generation Time: May 29, 2018 at 07:11 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 5.6.35
 
@@ -39,16 +39,21 @@ CREATE TABLE `orders` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(255) NOT NULL,
   `product_img_name` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL,
+  `shipping` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `product_code`, `product_name`, `product_desc`, `price`, `units`, `total`, `date`, `email`, `product_img_name`, `status`) VALUES
-(22, 'ABL', 'Badshavog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 47, 3, 141, '2018-05-20 18:34:43', 'tasdid@hossain.com', 'rice2badshavog.jpg', 0),
-(23, 'millstone', 'millstone', 'The Sports Band collection features highly polished stainless steel and space black stainless steel cases. The display is protected by sapphire crystal. And there is a choice of three different leather bands.', 1000, 3, 3000, '2018-05-20 19:02:50', 'tasdid@hossain.com', 'fish3ilish.jpg', 0);
+INSERT INTO `orders` (`id`, `product_code`, `product_name`, `product_desc`, `price`, `units`, `total`, `date`, `email`, `product_img_name`, `status`, `shipping`) VALUES
+(22, 'ABL', 'Badshavog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 47, 3, 141, '2018-05-20 18:34:43', 'tasdid@hossain.com', 'rice2badshavog.jpg', 0, ''),
+(23, 'millstone', 'millstone', 'The Sports Band collection features highly polished stainless steel and space black stainless steel cases. The display is protected by sapphire crystal. And there is a choice of three different leather bands.', 1000, 3, 3000, '2018-05-20 19:02:50', 'tasdid@hossain.com', 'fish3ilish.jpg', 0, ''),
+(24, 'ABL', 'Badshavog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 47, 3, 141, '2018-05-28 04:49:31', 'tasdid@hossain.com', 'rice2badshavog.jpg', 0, ''),
+(25, 'OSX', 'Tomato', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 26, 6, 156, '2018-05-28 04:58:44', 'tasdid@hossain.com', 'tomato.jpg', 0, ''),
+(26, 'OOP', 'Boro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 49, 5, 245, '2018-05-28 07:50:06', 'tasdid@hossain.com', 'rice6boro.jpg', 0, ''),
+(27, 'aaX', 'miniket', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 47, 3, 141, '2018-05-29 05:06:55', 'tasdid@hossain.com', 'rice5miniket.jpg', 0, '');
 
 -- --------------------------------------------------------
 
@@ -77,11 +82,11 @@ INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `p
 (3, 'millstone', 'millstone', 'The Sports Band collection features highly polished stainless steel and space black stainless steel cases. The display is protected by sapphire crystal. And there is a choice of three different leather bands.', 'fish3ilish.jpg', 30, '1000.00', 'fish'),
 (4, 'GBL', 'Katari Vog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice1.jpg', 4, '48.00', 'rice'),
 (5, 'BBL', 'Najirshal', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice1najirshal.jpg', 0, '52.00', 'rice'),
-(6, 'ABL', 'Badshavog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 'rice2badshavog.jpg', 11, '47.00', 'rice'),
+(6, 'ABL', 'Badshavog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 'rice2badshavog.jpg', 8, '47.00', 'rice'),
 (7, 'OBL', 'Balam', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill.', 'rice4balam.jpg', 20, '57.00', 'rice'),
 (8, 'QBL', 'Balam', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice4balam.jpg', 21, '42.00', 'rice'),
-(9, 'aaX', 'miniket', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice5miniket.jpg', 10, '47.00', 'rice'),
-(10, 'OOP', 'Boro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice6boro.jpg', 40, '49.00', 'rice'),
+(9, 'aaX', 'miniket', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice5miniket.jpg', 7, '47.00', 'rice'),
+(10, 'OOP', 'Boro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice6boro.jpg', 35, '49.00', 'rice'),
 (11, 'AAG', 'Chinigura', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'rice7chinigura.jpeg', 12, '60.00', 'rice'),
 (12, 'AAA', 'Brinjal ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'bagun.jpg', 50, '48.00', 'vegetable'),
 (13, 'CAG', 'Carrot', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'carrot.jpg', 14, '23.00', 'vegetable'),
@@ -94,6 +99,25 @@ INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `p
 (20, 'ISO', 'Chicken', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'chicken.jpg', 80, '150.00', 'meat'),
 (21, 'ISW', 'Steak', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'steak.jpg', 40, '800.00', 'meat'),
 (22, 'SAE', 'Goat ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue ultrices lorem, non suscipit risus ullamcorper ut. Morbi luctus eros leo, vitae feugiat urna fringill', 'goat.jpg', 50, '640.00', 'meat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promo`
+--
+
+CREATE TABLE `promo` (
+  `p_id` int(11) NOT NULL,
+  `promocode` varchar(5) NOT NULL,
+  `promo_value` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `promo`
+--
+
+INSERT INTO `promo` (`p_id`, `promocode`, `promo_value`) VALUES
+(1, 'QWERT', 20);
 
 -- --------------------------------------------------------
 
@@ -120,22 +144,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `fname`, `lname`, `address`, `city`, `pin`, `email`, `password`, `type`) VALUES
 (1, 'Blah', 'Bleh', 'Bangladesh', 'Dhaka', 95014, 'admin@farmtodesk.com', 'farmers', 'admin'),
 (3, 'aasd', 'asd', 'asd', 'asd', 1233, 'aa@aa.com', '123', 'user'),
-(4, 'Tasdid', 'Hossain', '234', 'Dhaka', 12544, 'tasdid@hossain.com', '123', 'user');
+(4, 'Tasdid', 'Hossain', '234', 'Dhaka', 12544, 'tasdid@hossain.com', '123', 'user'),
+(5, 'Admin', 'Main', '000', 'Dhaka', 7, 'mainadmin@farmtodesk.com', 'farmers', 'admin');
 
-
-
-
-CREATE TABLE `promo` (
-`p_id` int(11) NOT NULL,
-`promocode` varchar(5) NOT NULL,
-`promo_value` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
- 
-ALTER TABLE `promo`
-ADD PRIMARY KEY (`p_id`);
-
-
-INSERT INTO `promo` (`p_id`, `promocode`, `promo_value`) VALUES(1, 'QWERT', 20);
+--
 -- Indexes for dumped tables
 --
 
@@ -153,6 +165,12 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `product_code` (`product_code`);
 
 --
+-- Indexes for table `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`p_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -167,7 +185,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -179,7 +197,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
